5032888c8da355f45938b38e56f077e28b579c64
Daniel Salazar <juan.salazar111111@soy.sena.edu.co>
2025-04-05 13:41:10 -0500

    Solucionando git

---
+++ back/src/models/Usuario/user.js
@@ -0,0 +1,26 @@
+// models/Usuario/user.js
+import { Schema, model } from "mongoose";
+
+const usuarioSchema = new Schema(
+    {
+        nombre: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        passwordUser: {
+            type: String,
+            required: true,
+        },
+    },
+    { collection: "user" }
+);
+
+export default model("Usuario", usuarioSchema);
