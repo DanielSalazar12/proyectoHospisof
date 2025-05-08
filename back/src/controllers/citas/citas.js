543ac0df3d0014ce311c0ddd054ce01c298d3d5e
ADSO <juandaniels305@gmail.com>
2025-05-08 11:31:15 -0500

    Avance de dia

---
+++ back/src/controllers/citas/citas.js
@@ -0,0 +1,47 @@
+import Citas from "../../models/citas/citas";
+import { Types } from "mongoose";
+
+export const getAll = async () => {
+  try {
+    let listaCita = await Citas.find().exec();
+    res.status(200).send({
+      exito: true,
+      listaCita,
+    });
+  } catch (error) {
+    res.status(500).send({
+      exito: false,
+      mensaje: "Error en la consulta",
+    });
+  }
+};
+
+export const nuevo = async (req, res) => {
+  let datos = {
+    pacienteId: req.body.pacienteId,
+    medicoId: req.body.medicoId,
+    fechaCita: req.body.fechaCita,
+    horaCita: req.body.horaCita,
+    motivo: req.body.motivo,
+    notas: req.body.notas,
+    tipoConsulta: req.body.tipoConsulta,
+    fechaCreacion: req.body.fechaCreacion,
+    status: req.body.status || 1,
+  };
+
+  try {
+    const citaNuevo = new Citas(datos);
+    await citaNuevo.save();
+
+    return res.send({
+      estado: true,
+      mensaje: "Inserción exitosa",
+      usuario: usuarioNuevo,
+    });
+  } catch (error) {
+    return res.send({
+      estado: false,
+      mensaje: `Ha ocurrido un error en la consulta: ${error}`,
+    });
+  }
+};
---
+++ back/src/models/citas/citas.js
@@ -0,0 +1,46 @@
+import { Schema, model, Types } from "mongoose"; // Importa Types aquí
+
+const citasSchema = new Schema(
+  {
+    pacienteId: {
+      type: Types.ObjectId,
+      ref: "pacientes",
+      required: true,
+    },
+    medicoId: {
+      type: Types.ObjectId,
+      ref: "Medical",
+      required: true,
+    },
+    fechaCita: {
+      type: Date,
+      required: true,
+    },
+    horaCita: {
+      type: String,
+      required: true,
+    },
+    motivo: {
+      type: String,
+      required: true,
+    },
+    notas: {
+      type: String,
+    },
+    tipoConsulta: {
+      type: String,
+      required: true,
+    },
+    fechaCreacion: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+    },
+  },
+  { collection: "citas" }
+);
+
+export default model("Citas", citasSchema);
